98ac1804337d930eff5a18839b0511bd12b5d9d1
metalalchemist <cesar.verastegui17@gmail.com>
2025-06-21 17:13:46 -0700

    corregido error al acceder a instalación de voces piper

--- new/utils/app_utilitys.py
+++ new/utils/app_utilitys.py
@@ -1,9 +1,10 @@
 # -*- coding: cp1252
 from __future__ import unicode_literals
-from setup import reader
 from TTS.lector import detect_onnx_models
 from TTS.list_voices import install_piper_voice
+from setup import reader
 from ui.dialog_response import response
+from globals.data_store import config
 import sys, os,wx
 
 def restart_program():
@@ -19,6 +20,7 @@ def restart_program():
     os.execv(sys.executable, args)
 def porcentaje_a_escala(porcentaje): return 2.00 + (1 - ((porcentaje - -10) / (10 - -10))) * (0.50 - 2.00)
 def configurar_piper(carpeta_voces):
+    global config
     onnx_models = detect_onnx_models(carpeta_voces)
     if onnx_models is None:
         if response(_('Necesitas al menos una voz para poder usar el sintetizador Piper. ¿Quieres abrir nuestra carpeta de Drive para descargar algunos modelos? Si pulsas sí, se abrirá nuestra carpeta seguido de una ventana para instalar una una vez la descargues.'), _("No hay voces instaladas"),wx.YES_NO | wx.ICON_ASTERISK) == wx.ID_YES:
