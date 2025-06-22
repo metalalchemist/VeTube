a1b98b8646f469f0610f707b89249573d6c3d28d
metalalchemist <cesar.verastegui17@gmail.com>
2025-06-21 17:36:07 -0700

    comprueba si hay piper, inicializa lector con los atributos de  configuración.

--- new/run_main_window.py
+++ new/run_main_window.py
@@ -1,10 +1,20 @@
 import wx
 import setup
 from globals.data_store import config
+from globals.resources import carpeta_voces,lista_voces_piper
 from controller.main_controller import MainController
 from update import updater,update
+from TTS.lector import detect_onnx_models
+from utils.app_utilitys import configurar_piper
 def run_app():
     app = wx.App(False)
+    if config['sistemaTTS'] == "piper":
+        if detect_onnx_models(carpeta_voces) is not None:
+            setup.reader._lector=setup.reader._lector.piperSpeak(f"piper/voices/voice-{lista_voces_piper[config['voz']][:-5]}/{lista_voces_piper[config['voz']]}")
+            lector_Salidas = setup.reader._lector.get_devices()
+            salida_actual = setup.reader._lector.find_device_id(setup.player.devicenames[config["dispositivo"]-1])
+            setup.reader._lector.set_device(salida_actual)
+        configurar_piper(carpeta_voces)
     if config['donations']:
         update.donation()
     if config.get('updates', False):
--- new/setup.py
+++ new/setup.py
@@ -5,3 +5,7 @@ from helpers.reader_handler import ReaderHandler
 languageHandler.setLanguage(config['idioma'])
 player=playsound()
 reader = ReaderHandler()
+reader._leer.set_rate(config['speed'])
+reader._leer.set_pitch(config['tono'])
+reader._leer.set_voice(reader._leer.list_voices()[config['voz']])
+reader._leer.set_volume(config['volume'])
