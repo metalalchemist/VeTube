b8e3d5c73372e1eaefddc3aed6863fb40328d125
metalalchemist <cesar.verastegui17@gmail.com>
2025-07-07 16:14:36 -0700

    mejorar a la ui (IA)

--- new/ui/editor/editor.py
+++ new/ui/editor/editor.py
@@ -7,30 +7,35 @@ class EditorCombinaciones:
         self.dlg_teclado = wx.Dialog(parent, wx.ID_ANY, _("Editor de combinaciones de teclado para Vetube"))
         sizer = wx.BoxSizer(wx.VERTICAL)
         label_editor = wx.StaticText(self.dlg_teclado, wx.ID_ANY, _("&Selecciona la combinación de teclado a editar"))
-        self.combinaciones = wx.ListCtrl(self.dlg_teclado, wx.ID_ANY, style=wx.LC_REPORT)
-        self.combinaciones.InsertColumn(0, _("acción: "))
-        self.combinaciones.InsertColumn(1, _("combinación de teclas: "))
-        for i in range(len(mensaje_teclas)): self.combinaciones.InsertItem(i, mensaje_teclas[i])
-        c=0
+        self.combinaciones = wx.ListCtrl(self.dlg_teclado, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
+        self.combinaciones.InsertColumn(0, _("acción: "), width=200)
+        self.combinaciones.InsertColumn(1, _("combinación de teclas: "), width=200)
+        for i in range(len(mensaje_teclas)):
+            self.combinaciones.InsertItem(i, mensaje_teclas[i])
+        c = 0
         for valor in editor.teclas:
             self.combinaciones.SetItem(c, 1, valor)
-            c+=1
+            c += 1
         self.combinaciones.Focus(0)
         self.combinaciones.SetFocus()
+
         self.editar = wx.Button(self.dlg_teclado, -1, _(u"&Editar"))
         self.editar.SetDefault()
         self.restaurar = wx.Button(self.dlg_teclado, -1, _(u"&restaurar combinaciones por defecto"))
         self.close = wx.Button(self.dlg_teclado, wx.ID_CANCEL, _(u"&Cerrar"))
-        firstSizer = wx.BoxSizer(wx.HORIZONTAL)
-        firstSizer.Add(label_editor, 0, wx.ALL, 5)
-        firstSizer.Add(self.combinaciones, 0, wx.ALL, 5)
-        secondSizer = wx.BoxSizer(wx.HORIZONTAL)
-        secondSizer.Add(self.editar, 0, wx.ALL, 5)
-        secondSizer.Add(self.restaurar, 0, wx.ALL, 5)
-        secondSizer.Add(self.close, 0, wx.ALL, 5)
-        sizer.Add(firstSizer, 0, wx.ALL, 5)
-        sizer.Add(secondSizer, 0, wx.ALL, 5)
+
+        sizer.Add(label_editor, 0, wx.ALL, 5)
+        sizer.Add(self.combinaciones, 1, wx.EXPAND | wx.ALL, 5)
+
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        button_sizer.AddStretchSpacer()
+        button_sizer.Add(self.editar, 0, wx.ALL, 5)
+        button_sizer.Add(self.restaurar, 0, wx.ALL, 5)
+        button_sizer.Add(self.close, 0, wx.ALL, 5)
+        sizer.Add(button_sizer, 0, wx.EXPAND | wx.ALL, 5)
+
         self.dlg_teclado.SetSizerAndFit(sizer)
+        self.dlg_teclado.SetSize((500, 400))
         self.dlg_teclado.Centre()
 
     def ShowModal(self):
