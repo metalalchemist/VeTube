05bcac344119963524034616e7549dca6cafa762
metalalchemist <cesar.verastegui17@gmail.com>
2025-08-26 10:41:25 -0700

    released 3.1

--- update/updater.py
+++ update/updater.py
@@ -8,7 +8,7 @@ from accessible_output2.outputs import auto
 logger = logging.getLogger("updater")
 Lector=auto.Auto()
 def do_update(endpoint='https://raw.githubusercontent.com/metalalchemist/VeTube/master/updater.json'):
-    try: result = update.perform_update(endpoint=endpoint, current_version="3.0", app_name="VeTube", update_available_callback=available_update_dialog, progress_callback=progress_callback, update_complete_callback=update_finished)
+    try: result = update.perform_update(endpoint=endpoint, current_version="3.1", app_name="VeTube", update_available_callback=available_update_dialog, progress_callback=progress_callback, update_complete_callback=update_finished)
     except Exception as e:
         Lector.speak(_("Hubo un error al actualizar VeTube.")+str(e))
     return result
